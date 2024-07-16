a2f539dd9968796ffb945ecf83bb72a76642d99a
ChingizPro <chingizprog@gmail.com>
2024-07-16 13:56:58 +0500

    Display platform icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,17 +1,41 @@
-import { Text } from '@chakra-ui/react';
+import {
+  FaWindows,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaXbox,
+  FaAndroid,
+} from 'react-icons/fa';
+import { MdPhoneIphone } from 'react-icons/md';
+import { BsNintendoSwitch } from 'react-icons/bs';
+import { BsGlobe } from 'react-icons/bs';
+import { Icon, HStack } from '@chakra-ui/react';
 import { Platform } from '../hooks/useGames';
+import { IconType } from 'react-icons';
 
 interface Props {
   platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
-  console.log(platforms);
+  const iconMap: {[key: string]: IconType} = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    android: FaAndroid,
+    mac: FaApple,
+    linux: FaLinux,
+    nintendo: BsNintendoSwitch,
+    ios: MdPhoneIphone,
+    web: BsGlobe
+  }
 
   return (
-    <>
-      {platforms.map(platform => <Text key={platform.id}>{platform.name}</Text>)}
-    </>
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon color={'gray.500'} fontSize={20} as={iconMap[platform.slug]} key={platform.id}></Icon>
+      ))}
+    </HStack>
   );
 };
 
