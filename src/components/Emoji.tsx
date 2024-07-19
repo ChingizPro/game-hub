b7d205a50a69efc36a5be00f0368d6b35f68d00e
ChingizPro <chingizprog@gmail.com>
2024-07-20 01:29:06 +0500

    Add emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,20 @@
+import { Image, ImageProps } from '@chakra-ui/react';
+import bullsEye from '../assets/bulls-eye.webp';
+import thumsUp from '../assets/thumbs-up.webp';
+import meh from '../assets/meh.webp';
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+  const emojiMap: {[key: number]: ImageProps} = {
+    5: { src: bullsEye, alt: 'recommended', boxSize: '40px' },
+    4: { src: thumsUp, alt: 'exceptional', boxSize: '32px' },
+    3: { src: meh, alt: 'meh', boxSize: '32px' },
+  };
+  return <Image marginTop='5px' {...emojiMap[rating]} />;
+};
+
+export default Emoji;
