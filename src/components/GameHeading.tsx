0f778816d6c9115b2a890377dc35137aee9796f3
ChingizPro <chingizprog@gmail.com>
2024-08-08 13:12:59 +0500

    Refactor- simplify game query

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,14 +1,22 @@
 import { Heading } from '@chakra-ui/react';
 import { GameQuery } from '../App';
+import useGenres from '../hooks/useGenres';
+import usePlatforms from '../hooks/usePlatforms';
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  let heading = `${gameQuery?.platform?.name || ''} ${
-    gameQuery?.genre?.name || ''
-  } Games`;
+  const { data: genres } = useGenres();
+  let genre = genres?.results.find((g) => g.id === gameQuery.genreId);
+
+  const { data: platforms } = usePlatforms();
+  let selectedPlatform = platforms.results.find(
+    (p) => p.id === gameQuery.platformId
+  );
+
+  let heading = `${selectedPlatform?.name || ''} ${genre?.name || ''} Games`;
   return (
     <Heading fontSize='44px' as='h1' marginBottom={5}>
       {heading}
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -13,10 +13,10 @@ import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
 
-const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenreId }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -46,7 +46,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
                 borderRadius='5px'
               />
               <Button
-                fontWeight={selectedGenre?.id === genre.id ? 'bold' : 'normal'}
+                fontWeight={selectedGenreId === genre.id ? 'bold' : 'normal'}
                 textAlign='left'
                 whiteSpace='normal'
                 onClick={() => onSelectGenre(genre)}
